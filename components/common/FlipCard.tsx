f329d5a04d4d59af51d7979fbc993d0ad908554d
CryptoNNja <ddpludo@tutanota.com>
2026-01-16 22:38:59 +0100

    fix: force FlipCard height to 190px (not minHeight)
    - Changed minHeight to height for consistent sizing
    
    - All 4 FlipCards now have exact same height
    
    - Ensures uniform appearance in 1x5 grid layout

--- components/common/FlipCard.tsx
+++ components/common/FlipCard.tsx
@@ -50,7 +50,7 @@ export const FlipCard = ({
       className="flip-card-container"
       style={{ 
         perspective: '1000px',
-        minHeight: '190px'
+        height: '190px'
       }}
     >
       <div
